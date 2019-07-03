4480d80f6ee8aec74c081f221fb161559f2a6938
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-07-03 17:25:01 +0100

    Battleships progress

---
+++ Writing Real Code/Battleships/script.js
@@ -0,0 +1,45 @@
+var isSunk = false;
+var location1;
+var location2;
+var location3;
+var guess;
+var guessesRemaining = 3;
+var hits = 0;
+
+location1 = 4;
+location2 = location1 + 1;
+location3 = location2 + 1;
+
+
+while (hits < 3 && guessesRemaining > 0) {
+guess = prompt("Fire! (0-6):");
+  if (guess > 6) {
+    alert("Too Far!");
+  } else if (guess < 0) {
+    alert("Too Close!");
+  } else if (guess == location1) {
+    alert("Hit!");
+    location1 = null;
+    hits++;
+    guessesRemaining--;
+  } else if (guess == location2) {
+    alert("Hit!");
+    location2 = null;
+    hits++;
+    guessesRemaining--;
+  } else if (guess == location3) {
+    alert("Hit!");
+    location3 = null;
+    hits++;
+    guessesRemaining--;
+  } else if (guess != location3 || location2 || location3) {
+    alert("Miss!");
+    guessesRemaining--;
+  };
+};
+
+if (hits == 3) {
+  alert("Win");
+}  else if (guessesRemaining == 0) {
+  alert("You Lose!");
+}
