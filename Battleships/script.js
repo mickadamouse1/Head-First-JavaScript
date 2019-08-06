b82faa5540c86db1d224b9736ab6b0ee284d79dc
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-08-06 18:49:43 +0100

    Battleships: fixed bug where battleships would become undefined if push outside of the grid

--- Battleships/script.js
+++ Battleships/script.js
@@ -1,51 +1,50 @@
-
-// THREE TWO CELL-LONG BATTLESHIPS
-
+var attackScript = document.createElement("script");
+attackScript.src = "attack.js";
+document.head.appendChild(attackScript);
 var arrCells = [];
 for (var i = 0; i < 64; i++) {
   arrCells[i] = i + 1
 }
 
-while (arrCells.length > 54){
-  var x = 7;
-  var ship1Limit = arrCells.splice(x, 1);
-  var arrShipLimit = [];
-  arrShipLimit.push(ship1Limit);
-  x = x + 8;
-  console.log("ship 1 limit is " + arrShipLimit);
-}
-
-
-console.log(arrCells);
-
 var ship1 = {
   assignLoc: function() {
-    this.loc1 = arrCells[Math.floor(Math.random() * 64)];
+    this.loc1 = generateHorizPos();
     this.loc2 = this.loc1 + 1;
   }
-}
+};
 
 var ship2 = {
   assignLoc: function() {
-    this.loc1 = arrCells[Math.floor(Math.random() * 64)];
+    this.loc1 = arrCells[Math.floor(Math.random() * 56)];
     this.loc2 = this.loc1 + 8;
-    if (this.loc1 == ship1.loc1 || this.loc2 == ship1.loc1) {
-      this.loc1--;
-      this.loc2--;
-      console.log(`This ship moved from ${this.loc1 + 1} to ${this.loc1}.`);
-    } else if (this.loc1 == ship1.loc2 || this.loc2 == ship1.loc2) {
-      this.loc1++;
-      this.loc2++;
-      console.log(`This ship moved from ${this.loc1 - 1} to ${this.loc1}.`);
+    adjustPos();
+  }
+};
+
+function generateHorizPos() {
+  var a = Math.floor(Math.random() * 7) + 1;
+  var b = a + 8;
+  var c = b + 8;
+  var d = c + 8;
+  var e = d + 8;
+  var f = e + 8;
+  var g = f + 8;
+  var h = g + 8;
+  var arr = [a, b, c, d, e, f, g, h];
+  return arr[Math.floor(Math.random() * 8)];
+}
+
+function adjustPos() {
+  if (ship2.loc1 == ship1.loc1 || ship2.loc1 == ship1.loc2 || ship2.loc2 == ship1.loc1 || ship2.loc2 == ship1.loc2) {
+    ship2.loc1 = ship1.loc2 + 1;
+    ship2.loc2 = ship2.loc1 + 8;
+    console.log("moved");
+    if (ship2.loc1 > 54) {
+      ship2.loc1 = ship1.loc1 - 9;
+      ship2.loc2 = ship2.loc1 + 8;
     }
   }
 }
-
-ship1.assignLoc();
-ship2.assignLoc();
-
-console.log(ship1.loc1, ship1.loc2);
-console.log(ship2.loc1, ship2.loc2);
 
 function displayShips(shipLoc) {
   ship1cell = shipLoc;
@@ -63,27 +62,31 @@ function displayShips(shipLoc) {
     ship1cell = "f" + (ship1cell - 40);
   } else if (ship1cell < 57) {
     ship1cell = "g" + (ship1cell - 48);
-  } else if (ship1cell < 64) {
+  } else if (ship1cell <= 64) {
     ship1cell = "h" + (ship1cell - 56);
   }
-  document.getElementById(ship1cell.toString()).classList.add("miss");
+  document.getElementById(ship1cell.toString()).classList.add("ship");
 }
 
 function checkHit(cell, id) {
   if (cell == ship1.loc1 || cell == ship1.loc2 || cell == ship2.loc1 || cell == ship2.loc2) {
     console.log("hit");
+    id.classList.add("hit");
+    id.innerHTML = "Hit";
   } else {
-    console.log("miss");
+    console.log("Attacked cell " + cell + " and missed");
     id.classList.add("miss");
-    id.innerHTML = "miss";
+    id.innerHTML = "Miss";
   }
 }
 
-e1.onclick = function () {
-  checkHit(43, e1);
-};
+ship1.assignLoc();
+ship2.assignLoc();
 
 displayShips(ship1.loc1);
 displayShips(ship1.loc2);
 displayShips(ship2.loc1);
 displayShips(ship2.loc2);
+
+console.log(ship1.loc1, ship1.loc2);
+console.log(ship2.loc1, ship2.loc2);
